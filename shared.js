b662ca36052076570bb67fd551da3c744cd3d637
Nikolas Nascimento Aguilar <nik.adcd@gmail.com>
2023-07-28 15:55:39 -0300

    practice animation function

--- shared.js
+++ shared.js
@@ -17,6 +17,7 @@ var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
+var ctaButton= document.querySelector('.main-nav__item--cta');
 
 // console.log(backdrop);
 // console.dir(selectPlanButtons);
@@ -70,3 +71,16 @@ toggleButton.addEventListener('click', function() {
     mobileNav.classList.add('open');
     backdrop.classList.add('open');
 });
+
+
+ctaButton.addEventListener('animationstart', function(event){
+    console.log('animation started',event);
+})
+
+ctaButton.addEventListener('animationend', function(event){
+    console.log('animation ended',event);
+})
+
+ctaButton.addEventListener('animationiteration', function(event){
+    console.log('animation iteration',event);
+})
